c31e22e4fd0a37ea2f7419b7275dc45fc1f9b4ae
Jon <kaimmej@gmail.com>
2024-04-01 08:51:57 -0700

    Update strings.py

---
+++ GUIDES/strings.py
@@ -0,0 +1,18 @@
+
+
+def main():
+
+
+    # 
+    #
+    # SubStrings
+    #   string[start:end:step]
+    string = "freeCodeCamp"
+
+    print(string[0:5])  # get the first 5 characters
+    print(string[-5:])  # get the last 5 characters
+    print(string[::2])  # get every other character
+
+
+
+main()
