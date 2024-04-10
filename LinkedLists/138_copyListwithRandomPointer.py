2c8c24b2fea83043a7c1dcb18185c285c2f7da9b
Jon <kaimmej@gmail.com>
2024-04-10 12:55:03 -0700

    fixed errors in linked list implementations

--- LinkedLists/138_copyListwithRandomPointer.py
+++ LinkedLists/138_copyListwithRandomPointer.py
@@ -1,11 +1,11 @@
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, x, next=None, random=None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
 
 class Solution(object):
     def copyRandomList(self, head):
