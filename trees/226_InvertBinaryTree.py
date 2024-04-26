3e2fcc46611f4f059cc774baaf43e5edc8598298
Jon <kaimmej@gmail.com>
2024-04-26 08:50:41 -0700

    MaximumDepth of Binary Tree

--- trees/226_InvertBinaryTree.py
+++ trees/226_InvertBinaryTree.py
@@ -11,3 +11,13 @@ class Solution(object):
         :rtype: TreeNode
         """
         
+        if root is None:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
