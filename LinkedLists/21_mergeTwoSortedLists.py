0f3f55d56a9fa939c80f79a4d7ee8e75fc537fca
kaimmej <kaimmej@gmail.com>
2025-04-18 14:04:32 -0700

    linked list cycle 2

--- LinkedLists/21_mergeTwoSortedLists.py
+++ LinkedLists/21_mergeTwoSortedLists.py
@@ -36,3 +36,42 @@ class Solution:
 
         dummy = dummy.next
         return dummy
+
+
+
+#
+#
+
+
+        l1 = list1
+        l2 = list2
+        dummy = node = ListNode()
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                dummy.next = l1
+                l1 = l1.next
+
+            else:
+                dummy.next = l2
+                l2 = l2.next
+            
+            dummy = dummy.next
+        
+        dummy.next = l1 or l2
+
+        # We need to keep track of the head. 
+        return node.next
+
+        # dummy = node = ListNode()
+
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         node.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         node.next = list2
+        #         list2 = list2.next
+        #     node = node.next
+
+        # node.next = list1 or list2
