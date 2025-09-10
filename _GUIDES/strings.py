1644677742ad999c23e8df51494b7cc537f1dbd1
kaimmej <jon.m.kaimmer@gmail.com>
2025-09-10 12:24:36 -0700

    adding string methods for filtering and mapping functions

--- _GUIDES/strings.py
+++ _GUIDES/strings.py
@@ -23,4 +23,26 @@ def main():
     print(string)
 
 
+
+    # Iterating, filtering, mapping... 
+    def isPalindrom(self, s: str) -> bool:
+
+        # FILTER
+        # filter returns an iterable filter object. 
+        #   - the function is applied to each element. if function(element) == True, the element is kept.
+        #   - result is a lazy iterator (not a string/list)
+        filtered_chars = filter(lambda ch: ch.isalnum(), s)
+
+
+        # MAP
+        # map applies the function to each element and returns a map object (another lazy iterator...)
+        # So map is being applied to each object that was returned by the first filtering function. 
+        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
+
+        filtered_chars_list = list(lowercase_filtered_chars)
+        reversed_chars_list = filtered_chars_list[::-1]
+
+        return filtered_chars_list == reversed_chars_list
+
+
 main()
