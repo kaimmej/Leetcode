0b3122cb050c2ce8e9790f4ba12e5a161aeb8638
kaimmej <kaimmej@gmail.com>
2025-09-30 09:35:32 -0700

    valid palindrome

--- TwoPointer/125_validPalindrome.py
+++ TwoPointer/125_validPalindrome.py
@@ -15,3 +15,29 @@ class Solution:
                 right -= 1
                 
         return True
+
+
+# STACK solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # Clean the input data... 
+        # Everything needs to be lowercase
+        input_lowerCase = s.lower()
+
+        # Remove all non-alpha characters 
+        input_alphaLowerCase = re.sub(r'[^A-Za-z0-9]', '', input_lowerCase) # This is saying "anything not a alpha character, replace with empty "" "
+        s = []
+        # Add everything to a stack... 
+        for i in range(len(input_alphaLowerCase)):
+            # Add to the stack
+            s.append(input_alphaLowerCase[i])
+        
+        # Iterate and compare characters... forwards and backwards
+        for i in range(len(input_alphaLowerCase)):
+            end_character = s.pop()
+            front_character = input_alphaLowerCase[i]
+            if end_character != front_character:
+                return False
+        return True
+        
